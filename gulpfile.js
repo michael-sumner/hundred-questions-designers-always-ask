e557b15c4152c8242bd548a1624bd8e9a6a4a539
Mic Sumner <mic@Mics-MacBook-Pro.local>
2020-10-21 10:57:02 +0100

    remove unnecessary comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-// generated on 2017-08-28 using generator-webapp 3.0.1
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
